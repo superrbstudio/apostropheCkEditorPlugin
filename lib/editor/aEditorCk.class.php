dd82b3f0e81dc9b3ba17566375694c9cecda0970
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-04-30 23:39:11 +0000

    * Workaround for http://dev.ckeditor.com/ticket/5342 allows editor to be properly narrow in the sidebar * Workaround for http://dev.ckeditor.com/ticket/4049 is... always using the v2 skin for now. Bummer, since v2 doesn't support specifying a background color * More configurability, you should be able to override everything that can be done with ckeditor configuration in app.yml
    It's pretty good now, if a little dull looking due to v2 skin
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheCkEditorPlugin/trunk@3606 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/editor/aEditorCk.class.php
+++ lib/editor/aEditorCk.class.php
@@ -80,11 +80,19 @@ class aEditorCk extends aEditor
     // ckEditor's "find myself and bring myself in via a script tag" support is confused by Symfony's layout,
     // so we do it ourselves in view.yml (for now; we'll do it better)
     $ckEditor->initialized = true;
-    
-    // We have to clobber the editor if CKEditor thinks it already exists. It would be nice to reuse it
-    // but we already pulled it out of the DOM to work with Apostrophe's refresh model for edit views.
-    // http://stackoverflow.com/questions/2310111/uncaught-ckeditor-editor-the-instance-html-already-exists
 
+    // You can configure anything ckeditor allows by specifying a full config array. The other options
+    // are applied after that so you can still use them
+    $ckEditor->config = array_merge($ckEditor->config, sfConfig::get('app_a_ckEditor_config', array()));
+
+    // ACHTUNG: the kama skin has a bug that makes everything 12px too wide, see ckeditor bug #4049.
+    // Otherwise we'd use it to get uiColor
+    $ckEditor->config['skin'] = sfConfig::get('app_a_ckEditor_skin', 'v2');
+
+    // Sadly this works only with the kama skin. Not a bug, that's how it is in ckeditor land
+    $ckEditor->config['uiColor'] = sfConfig::get('app_a_ckEditor_uiColor', '#ffffff');
+    $uiColor = $ckEditor->config['uiColor'];
+    
     if (isset($options['width']))
     {
       $ckEditor->config['width'] = $options['width'];
@@ -105,8 +113,38 @@ class aEditorCk extends aEditor
 
     $ckEditor->config['toolbar'] = $toolbar;
     $ckEditor->config['format_tags'] = sfConfig::get('app_a_ckEditor_format_tags', 'p;h3;h4;h5;h6;pre');
+    // We have to clobber the editor if CKEditor thinks it already exists. It would be nice to reuse it
+    // but we already pulled it out of the DOM to work with Apostrophe's refresh model for edit views.
+    // http://stackoverflow.com/questions/2310111/uncaught-ckeditor-editor-the-instance-html-already-exists
 
     $content = "<script type=\"text/javascript\" charset=\"utf-8\">if (CKEDITOR.instances['$name']) { delete CKEDITOR.instances['$name'] };</script>";
+// This is an interesting idea but I don't have the styles quite right, there's a 1px bump in the thickness of things as
+// I hover them. Also the styles ought to be specific to the individual instance to avoid busting all other instances if
+// they are colored differently. Sure wish they'd just fix ckeditor #4049!
+//     if ($ckEditor->config['skin'] === 'v2')
+//     {
+//       // We trick the v2 skin into looking the way we styled FCK.
+//       // It doesn't support a uiColor option natively like kama does
+//       // (and kama has a width bug so we can't use it)
+//       $content .= <<<EOM
+// <style>
+// .cke_skin_v2 .cke_top, .cke_skin_v2 .cke_bottom, .cke_shared .cke_skin_v2, .cke_skin_v2 .cke_label, .cke_skin_v2 .cke_button .cke_off
+// {
+//   background-color: $uiColor !important;
+//   border: 1px solid $uiColor !important;
+// }
+// .cke_skin_v2 .cke_label, .cke_skin_v2 .cke_button .cke_off
+// {
+//   border: 1px solid $uiColor !important;
+// }
+// .cke_skin_v2 .cke_label :hover, .cke_skin_v2 .cke_button .cke_off :hover
+// {
+//   border: 1px solid blue !important;
+// }
+// </style>
+// EOM
+// ;
+//     }
     $content .= $ckEditor->editor($options['name'], $value);
 
     // Skip the braindead 'type='text'' hack that breaks Safari
@@ -125,6 +163,17 @@ $(function() {
     var value = CKEDITOR.instances['$name'].getData();
     apostrophe.log(value);
     textarea.val(value);
+  });
+  CKEDITOR.instances['$name'].on('instanceReady', function (evt) 
+  {
+    var editor = evt.editor;
+    // http://cksource.com/forums/viewtopic.php?p=48574#p48574
+    if (CKEDITOR.env.webkit && parseInt(editor.config.width) < 310) 
+    {
+      var iframe = document.getElementById('cke_contents_' + editor.name).firstChild;
+      iframe.style.display = 'none';
+      iframe.style.display = 'block';
+    }
   });
 });
 </script>
